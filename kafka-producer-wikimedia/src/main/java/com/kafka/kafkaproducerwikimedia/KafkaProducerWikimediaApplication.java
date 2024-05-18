569ba7d67a5ca67e81fe531956f9cb8caa4492c9
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-05-18 16:08:39 +0530

    Added code for getting the changes from wikimedia

--- kafka-producer-wikimedia/src/main/java/com/kafka/kafkaproducerwikimedia/KafkaProducerWikimediaApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/kafka/kafkaproducerwikimedia/KafkaProducerWikimediaApplication.java
@@ -1,13 +1,20 @@
 package com.kafka.kafkaproducerwikimedia;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaProducerWikimediaApplication {
+public class KafkaProducerWikimediaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerWikimediaApplication.class, args);
 	}
-
+	@Autowired
+	private WikimediaChangesProducer wikimediaChangesProducer;
+	@Override
+	public void run(String... args) throws Exception {
+		wikimediaChangesProducer.sendMessage();
+	}
 }
